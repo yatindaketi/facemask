8da8f40d238a73c6dcb99a0e381b49c4c06e7976
yatin2901 <79344352+yatin2901@users.noreply.github.com>
2021-12-20 16:22:26 +0530

    Update detect_mask_video.py

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -4,25 +4,25 @@ import numpy as np
 
 model = load_model("C:/Users/DELL/Desktop/facemask/dataset/mask_detector.model")
 
-face_clsfr=cv2.CascadeClassifier(cv2.data.haarcascades+"C:\\Users\\DELL\\Desktop\\facemask\\dataset\\haarcascade_frontalface_default.xml")
+face_clsfr=cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
 
-#source=cv2.VideoCapture(0)
+source=cv2.VideoCapture(0)
 
 labels_dict={0:'MASK',1:'NO MASK'}
 color_dict={0:(0,255,0),1:(0,0,255)}
 
 while(True):
 
-    img=cv2.imread("C:/Users/DELL/Downloads/lolplz.jpeg")
+    ret,img=source.read()
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces=face_clsfr.detectMultiScale(gray,1.3,5)  
 
     for (x,y,w,h) in faces:
     
         face_img=gray[y:y+w,x:x+w]
-        resized=cv2.resize(face_img,(100,100))
+        resized=cv2.resize(face_img,(672,672))
         normalized=resized/255.0
-        reshaped=np.reshape(normalized,(1,100,100,1))
+        reshaped=np.reshape(normalized,(3,224,224,3))
         result=model.predict(reshaped)
 
         label=np.argmax(result,axis=1)[0]
@@ -39,4 +39,4 @@ while(True):
         break
         
 cv2.destroyAllWindows()
-#source.release()
+source.release()
